57d90f8d2bc14a20a2b0a4dd79a9ff2cf0e1c039
Pelooton <115213936+Pelooton@users.noreply.github.com>
2022-02-24 14:09:59 +0000

    add yarn lock

--- src/components/MarketFavor.vue
+++ src/components/MarketFavor.vue
@@ -2,4 +2,6 @@
   <div class="product-list pb-5 vh-100" v-if="likeList.length>0">
     <div class="table">
       <table class="w-100">
-  
+        <thead class="thead">
+          <tr class="row m-0 text-light">
+            <th class="col-
