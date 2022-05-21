6d4d38054b4dfc737bc75bdbeeee0d149df56a25
Pelooton <115213936+Pelooton@users.noreply.github.com>
2022-05-21 17:29:20 +0000

    animations

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -2,4 +2,5 @@
   <div class="py-4">
     <nav aria-label="Page navigation example">
       <ul class="pagination justify-content-end m-0">
-        <li cla
+        <li class="page-item me-1 disabled">
+          <a class="page-link" :class="{'disabled': pa
