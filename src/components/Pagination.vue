f47fa5972eee3074edcf0c805bedf10345a14176
Pelooton <115213936+Pelooton@users.noreply.github.com>
2022-05-22 20:03:08 +0000

    prepare for next release

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -5,3 +5,5 @@
         <li class="page-item me-1 disabled">
           <a class="page-link" :class="{'disabled': pagination.currentPage===1}"><i class="bi bi-caret-left-fill"></i></a>
         </li>
+        <li class="page-item mx-1" v-for="page in pagination.totalPages" :key="page"
+       
