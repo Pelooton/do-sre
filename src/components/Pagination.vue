6830158e0834a859ef23fba030f01f84aa5f2328
Pelooton <115213936+Pelooton@users.noreply.github.com>
2022-05-22 18:55:05 +0000

    Removed unused field

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -3,4 +3,5 @@
     <nav aria-label="Page navigation example">
       <ul class="pagination justify-content-end m-0">
         <li class="page-item me-1 disabled">
-          <a class="page-link" :class="{'disabled': pa
+          <a class="page-link" :class="{'disabled': pagination.currentPage===1}"><i class="bi bi-caret-left-fill"></i></a>
+        </li>
