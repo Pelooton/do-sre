8850064cbc88b1cafe14a04a781d55e85bb03951
Pelooton <115213936+Pelooton@users.noreply.github.com>
2022-06-26 18:46:28 +0000

    Add requirements.txt.

--- src/main.js
+++ src/main.js
@@ -9,3 +9,19 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import BootstrapJs from 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { currency, toFixedTwo } from '@/methods/filters'
 import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+import aos from 'aos'
+import '../node_modules/aos/dist/aos.css'
+aos.init()
+
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
+app.use(VueAxios, axios)
+app.use(router)
+app.use(aos)
+app.config.globalProperties.BootstrapJs = BootstrapJs
+app.config.globalProperties.$filters = { currency, toFixedTwo }
+app.component('Loading', Loading)
+app.mount('#app')
