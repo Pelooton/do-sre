eadd7c9be88959e6a4eb3d13f3211ad55561f71d
Pelooton <115213936+Pelooton@users.noreply.github.com>
2022-08-29 13:07:06 +0000

    address code review comments

--- src/stores/walletStore.js
+++ src/stores/walletStore.js
@@ -1,3 +1,6 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
-import statusStore from './statusStore
+import statusStore from './statusStore'
+const status = statusStore()
+
+export default defineStore('wallet-data'
