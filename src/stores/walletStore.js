c0dfc49137ef9daca927a550be7238f0aba8c00e
Pelooton <115213936+Pelooton@users.noreply.github.com>
2022-09-04 08:18:24 +0000

    Updating readme file.

--- src/stores/walletStore.js
+++ src/stores/walletStore.js
@@ -3,4 +3,7 @@ import axios from 'axios'
 import statusStore from './statusStore'
 const status = statusStore()
 
-export default defineStore('wallet-data'
+export default defineStore('wallet-data', {
+  state: () => {
+    return {
+      stockList: JSON.parse(localStorage.getItem('stock')
