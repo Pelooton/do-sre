24a5b4de4ca0b2c99e4a47ed312f690b9d9e4bc8
Pelooton <115213936+Pelooton@users.noreply.github.com>
2022-11-19 11:45:58 +0000

    clean up logs

--- src/views/front/Home.vue
+++ src/views/front/Home.vue
@@ -1,4 +1,6 @@
 <template>
   <Navbar></Navbar>
   <div class="container-fluid home pb-4">
-    <div class="main-
+    <div class="main-pic row">
+      <div class="col-12 col-lg-6">
+        <img src="https://ppt.cc/ffEbK
