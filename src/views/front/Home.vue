4d524dc5a6caf86ec7f5652eb4038c192728d0bd
Pelooton <115213936+Pelooton@users.noreply.github.com>
2022-11-22 16:51:34 +0000

    merge?

--- src/views/front/Home.vue
+++ src/views/front/Home.vue
@@ -3,4 +3,6 @@
   <div class="container-fluid home pb-4">
     <div class="main-pic row">
       <div class="col-12 col-lg-6">
-        <img src="https://ppt.cc/ffEbK
+        <img src="https://ppt.cc/ffEbKx@.png" class="img-fluid p-5" alt="">
+      </div>
+      <div class="form co
