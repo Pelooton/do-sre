f9dff6bd3930cefbc21a5f42c6ff3d2c5249bad5
Pelooton <115213936+Pelooton@users.noreply.github.com>
2022-11-24 17:33:57 +0000

    Added 7 tweets

--- src/views/front/Home.vue
+++ src/views/front/Home.vue
@@ -6,4 +6,5 @@
         <img src="https://ppt.cc/ffEbKx@.png" class="img-fluid p-5" alt="">
       </div>
       <div class="form col-12 col-lg-6 align-self-center">
-        <h1 class="text-white" data-aos="fade-left
+        <h1 class="text-white" data-aos="fade-left" data-aos-duration="2000">在此平台交易和持有100種以上虛擬貨幣、追蹤市場價格變化</h1>
+        <form class="d-fle
