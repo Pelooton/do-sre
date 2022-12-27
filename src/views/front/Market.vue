1ebe4a83550945664cca7d0a95e70d4eb864896c
Pelooton <115213936+Pelooton@users.noreply.github.com>
2022-12-27 07:20:43 +0000

    Add phpdoc

--- src/views/front/Market.vue
+++ src/views/front/Market.vue
@@ -2,4 +2,5 @@
   <Navbar></Navbar>
   <div class="market">
     <div class="container-fluid px-5">
-      <nav class="
+      <nav class="pt-4" style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+        <ol cl
