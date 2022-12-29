f578785d47b985a77d3045c2d1fbb1cd2e9f0d7f
Pelooton <115213936+Pelooton@users.noreply.github.com>
2022-12-29 22:11:32 +0000

    fixed checkstyle

--- src/views/front/Market.vue
+++ src/views/front/Market.vue
@@ -5,4 +5,6 @@
       <nav class="pt-4" style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="#">主頁</a></li>
-          <li
+          <li class="breadcrumb-item active" aria-current="page">加密貨幣</li>
+        </ol>
+      </
