36ca83bae88d45298b905ad6b18ef0da41c00cc0
Pelooton <115213936+Pelooton@users.noreply.github.com>
2022-12-30 14:02:41 +0000

    GUI improvements

--- src/views/front/Market.vue
+++ src/views/front/Market.vue
@@ -8,4 +8,6 @@
           <li class="breadcrumb-item active" aria-current="page">加密貨幣</li>
         </ol>
       </nav>
-      <div class="top-nav pt-1 pb-4 d-flex justify-content
+      <div class="top-nav pt-1 pb-4 d-flex justify-content-between align-items-center">
+        <div class="category">
+          <router-link :to="{
