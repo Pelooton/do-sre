b2d2f546729962a057d2eec2005e9dd1c1d4a5bd
Pelooton <115213936+Pelooton@users.noreply.github.com>
2023-02-20 17:05:06 +0000

    Update theme.scss

--- src/views/front/Test.vue
+++ src/views/front/Test.vue
@@ -1,3 +1,8 @@
 <template>
   <ul>
-    <li v-for="(i, key) in sortData" :key="k
+    <li v-for="(i, key) in sortData" :key="key">1</li>
+  </ul>
+</template>
+
+<script>
+import cryptoStore from '@/stor
