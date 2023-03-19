a8c127b8056790fbb187ba92d394e8f6bd857c35
Pelooton <115213936+Pelooton@users.noreply.github.com>
2023-03-19 05:43:29 +0000

    Linux fix

--- src/views/front/Trade.vue
+++ src/views/front/Trade.vue
@@ -6,4 +6,9 @@
 
 <script>
 import Navbar from '@/components/Navbar.vue'
-import Footer from '@/co
+import Footer from '@/components/Footer.vue'
+export default {
+  components: { Navbar, Footer }
+}
+</script>
+
